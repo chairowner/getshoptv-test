1469b20f53b158e28e3c29913a9ca9793a9c127e
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-10-10 10:35:15 +0500

    create Partners block

--- app/modules/BalanceProfitUser/BalanceProfitUser.tsx
+++ app/modules/BalanceProfitUser/BalanceProfitUser.tsx
@@ -7,29 +7,31 @@ export const BalanceProfitUser = () => {
 	return (
 		<div className={classNames("wrapper", s.bg)}>
 			<div className="container">
-				<div className={s.container}>
-					<h2>Баланс между выручкой и удовлетворённостью пользователей</h2>
-					<div className={s.balanceBlock}>
-						<BalanceInfoBlock
-							title="Частотность под контролем"
-							text="Чтобы не ронять NPS и не увеличивать отток пользователей"
-						/>
-						<div className={s.logoWrapper}>
-							<div className={s.line} />
-							<div className={s.logo}>
-								<Image
-									src="/icons/balance_logo.svg"
-									alt="balance"
-									width={80}
-									height={74.81}
-								/>
+				<div className={s.wrapper}>
+					<div className={s.container}>
+						<h2>Баланс между выручкой и удовлетворённостью пользователей</h2>
+						<div className={s.balanceBlock}>
+							<BalanceInfoBlock
+								title="Частотность под контролем"
+								text="Чтобы не ронять NPS и не увеличивать отток пользователей"
+							/>
+							<div className={s.logoWrapper}>
+								<div className={s.line} />
+								<div className={s.logo}>
+									<Image
+										src="/icons/balance_logo.svg"
+										alt="balance"
+										width={80}
+										height={74.81}
+									/>
+								</div>
+								<div className={s.line} />
 							</div>
-							<div className={s.line} />
+							<BalanceInfoBlock
+								title="Максимальная выручка"
+								text="За счёт заполнения всех рекламных мест по высокой цене"
+							/>
 						</div>
-						<BalanceInfoBlock
-							title="Максимальная выручка"
-							text="За счёт заполнения всех рекламных мест по высокой цене"
-						/>
 					</div>
 				</div>
 			</div>
--- app/modules/Partners/Partners.tsx
+++ app/modules/Partners/Partners.tsx
@@ -1,18 +1,52 @@
 import Image from "next/image";
 import s from "./Partners.module.scss";
 
+interface IPartner {
+	src: string;
+	alt: string;
+	width: number;
+	height: number;
+}
+
+const partners: IPartner[] = [
+	{
+		width: 177,
+		height: 55,
+		alt: "clickwave logo",
+		src: "clickwave.svg",
+	},
+	{
+		width: 101.92,
+		height: 70.78,
+		alt: "zyphronix logo",
+		src: "zyphronix.svg",
+	},
+	{
+		width: 192.72,
+		height: 60.52,
+		alt: "novaspherelogo",
+		src: "novasphere.svg",
+	},
+];
+
 export const Partners = () => {
 	return (
 		<div className="wrapper">
 			<div className="container">
-				<div className={s.container}>
-					<div>
+				<div className={s.wrapper}>
+					<div className={s.container}>
 						<h3>Наши партнёры по монетизации</h3>
-					</div>
-					<div className={s.partners}>
-						<img src="/icons/partners/clickwave.svg" />
-						<img src="/icons/partners/zyphronix.svg" />
-						<img src="/icons/partners/novasphere.svg" />
+						<div className={s.partners}>
+							{partners.map((item, index) => (
+								<Image
+									key={index}
+									src={`/icons/partners/${item.src}`}
+									alt={item.alt}
+									width={item.width}
+									height={item.height}
+								/>
+							))}
+						</div>
 					</div>
 				</div>
 			</div>
